7f5e1ec71fe2a9f91a61cc28a9250906d5da3d12
SailerWoak <43933958+SailerWoak@users.noreply.github.com>
2018-10-24 21:34:57 +0300

    Update DelfiList.java

--- src/test/java/DelfiList.java
+++ src/test/java/DelfiList.java
@@ -10,10 +10,11 @@ import java.util.List;
 
 public class DelfiList {
     private final By ARTICLE_TITLE = By.xpath(".//a[@class ='top2012-title']"); //константа всегда пишется с (by = это локатор)
-    private final By MARTICLE_TITLE = By.xpath(".//a[@class ='md-scrollpos']");
+    private final By ARTICLE_TITLE_MOBILE = By.xpath(".//a[@class ='md-scrollpos']");
     private final String HOME_PAGE = "http://rus.delfi.lv";
-    private final String MHOME_PAGE = "http://m.rus.delfi.lv/";
+    private final String HOME_PAGE_MOBILE = "http://m.rus.delfi.lv/";
     WebDriver browser;
+    
     @Test
     public void delfiFirstTitleTest() {
         List<WebElement> webArticles = new ArrayList<WebElement>();
@@ -23,30 +24,22 @@ public class DelfiList {
         mybArticles.add("Из первых уст: 8 наблюдений о разнице жизни в Риге и Санкт-Петербурге");
         mybArticles.add("В расследовании убийства Бункуса Латвии помогут Польша, Германия и другие страны");
         mybArticles.add("Латвийский фигурист Васильев стал победителем соревнований в Минске");
-
-
+        
         System.setProperty("webdriver.chrome.driver", "D:/chromedriver.exe");
-
-        //Open browser
         browser = new ChromeDriver();
-
-        //Expend Browser
         browser.manage().window().maximize();
-
-        //Insert URL
         browser.get(HOME_PAGE);
-        //Get titles
+        
         webArticles = browser.findElements(ARTICLE_TITLE);
         Assertions.assertFalse(webArticles.isEmpty(),"Error");
           for (int i = 0; i < 5; i++) {
               Assertions.assertEquals(webArticles.get(i).getText(), mybArticles.get(i), "Error with 'for' ");
           }
-
-        browser.get(MHOME_PAGE);
-          webArticles = browser.findElements(MARTICLE_TITLE);
+        browser.get(HOME_PAGE_MOBILE);
+          webArticles = browser.findElements(ARTICLE_TITLE_MOBILE);
           Assertions.assertFalse(webArticles.isEmpty(), "Error for Mob.version");
-          for (int a = 0; a < 5; a++) {
-              Assertions.assertEquals(webArticles.get(a).getText(), mybArticles.get(a), "Error for Mob.version with 'for' ");
+          for (int i = 0; i < 5; i++) {
+              Assertions.assertEquals(webArticles.get(i).getText(), mybArticles.get(a), "Error for Mob.version with 'for' ");
           }
         }
     @AfterEach
@@ -55,12 +48,4 @@ public class DelfiList {
     }
 }
 
-
-
-       // for (String name : names){
-         //   System.out.println("Blame " + name);
-
-     //   for( int i = 0; i < name.size(); i++){
-       //     System.out.println("Girl "+ name.get(i));
-        //System.out.println(name.size(i));//кол-во записей в списке - size
 
